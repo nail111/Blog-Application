f7ba682e2bc4bbb7529236a7fd0344aaf66b6ab8
Nail <nailtagiyev1999@gmail.com>
2022-11-24 14:43:41 +0400

    add PostResponse class for paging and sorting

---
+++ src/main/java/com/blog/dto/PostResponse.java
@@ -0,0 +1,19 @@
+package com.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostResponse {
+    private List<PostDto> content;
+    private int pageNo;
+    private int pageSize;
+    private boolean last;
+    private Long totalElements;
+    private int totalPages;
+}
