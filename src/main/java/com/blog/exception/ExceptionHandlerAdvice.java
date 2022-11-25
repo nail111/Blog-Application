03592d4f8322f347f4c0b456ad6ba8093d5007d3
Nail <nailtagiyev1999@gmail.com>
2022-11-25 12:07:45 +0400

    modify exception handling

--- src/main/java/com/blog/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/blog/exception/ExceptionHandlerAdvice.java
@@ -1,20 +1,46 @@
 package com.blog.exception;
 
+import com.blog.dto.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(value = ResourceNotFoundException.class)
-    public final ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException) {
-        return new ResponseEntity<>(resourceNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    public final ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException,
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                resourceNotFoundException.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = BlogAPIException.class)
-    public final ResponseEntity<String> handleBlogAPIException(BlogAPIException blogAPIException) {
-        return new ResponseEntity<>(blogAPIException.getMessage(), blogAPIException.getStatus());
+    public final ResponseEntity<?> handleBlogAPIException(BlogAPIException blogAPIException, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                blogAPIException.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, blogAPIException.getStatus());
+    }
+
+    // handle Global exception
+    @ExceptionHandler(value = Exception.class)
+    public final ResponseEntity<?> handleGlobalException(Exception exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
