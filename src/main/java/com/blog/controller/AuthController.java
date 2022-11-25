442647849a34dd598ead4854196869b6488d4e52
Nail <nailtagiyev1999@gmail.com>
2022-11-25 19:05:58 +0400

    add Sign Up

--- src/main/java/com/blog/controller/AuthController.java
+++ src/main/java/com/blog/controller/AuthController.java
@@ -1,22 +1,34 @@
 package com.blog.controller;
 
 import com.blog.dto.LoginDto;
+import com.blog.dto.SignUpDto;
+import com.blog.model.Role;
+import com.blog.model.User;
+import com.blog.repo.RoleRepo;
+import com.blog.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginDto loginDto) {
@@ -29,5 +41,28 @@ public class AuthController {
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return ResponseEntity.ok("User signed-in successfully");
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto) {
+        if (userRepo.existsByUsername(signUpDto.getUsername())) {
+            return new ResponseEntity<>("Username is already taken", HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepo.existsByEmail(signUpDto.getEmail())) {
+            return new ResponseEntity<>("Email is already taken", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setEmail(signUpDto.getEmail());
+        user.setName(signUpDto.getName());
+        user.setUsername(signUpDto.getUsername());
+        user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        Role roles = roleRepo.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+
+        userRepo.save(user);
+        return new ResponseEntity<>("User registered successfully!!!", HttpStatus.CREATED);
     }
 }
