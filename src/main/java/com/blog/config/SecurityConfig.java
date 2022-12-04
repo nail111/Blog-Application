f0fd35f17edc00a0e1733807d9fe5654917a141b
Nail <nailtagiyev1999@gmail.com>
2022-12-04 14:11:06 +0400

    Give to Swagger some permissions

--- src/main/java/com/blog/config/SecurityConfig.java
+++ src/main/java/com/blog/config/SecurityConfig.java
@@ -49,6 +49,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                 .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/v3/api-docs/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/webjars/**").permitAll()
                 .anyRequest()
                 .authenticated();
 
