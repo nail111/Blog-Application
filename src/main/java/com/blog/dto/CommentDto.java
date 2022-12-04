36abe46c313c83d0bbbb52ef6cf7f7051950138d
Nail <nailtagiyev1999@gmail.com>
2022-12-04 20:38:07 +0400

    add @ApiModel and @ApiModelProperty annotations

--- src/main/java/com/blog/dto/CommentDto.java
+++ src/main/java/com/blog/dto/CommentDto.java
@@ -1,5 +1,7 @@
 package com.blog.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,17 +13,23 @@ import javax.validation.constraints.Size;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "Comment model information")
 public class CommentDto {
+
+    @ApiModelProperty(value = "Comment id")
     private Long id;
 
     @NotEmpty(message = "Name should not be null or empty")
+    @ApiModelProperty(value = "Comment name")
     private String name;
 
     @NotEmpty(message = "Email should not be null or empty")
     @Email
+    @ApiModelProperty(value = "Comment email")
     private String email;
 
     @NotEmpty
     @Size(min = 2, message = "Comment body should have at least 2 characters")
+    @ApiModelProperty(value = "Comment body")
     private String body;
 }
--- src/main/java/com/blog/dto/PostDto.java
+++ src/main/java/com/blog/dto/PostDto.java
@@ -1,5 +1,7 @@
 package com.blog.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,18 +13,26 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "Post model information")
 public class PostDto {
+
+    @ApiModelProperty(value = "Post id")
     private Long id;
 
     @NotEmpty
     @Size(min = 2, message = "Post title should have at least 2 characters")
+    @ApiModelProperty(value = "Post title")
     private String title;
 
     @NotEmpty
     @Size(min = 3, message = "Post description should have at least 3 characters")
+    @ApiModelProperty(value = "Post description")
     private String description;
 
     @NotEmpty
+    @ApiModelProperty(value = "Post content")
     private String content;
+
+    @ApiModelProperty(value = "Post comments")
     private Set<CommentDto> comments;
 }
